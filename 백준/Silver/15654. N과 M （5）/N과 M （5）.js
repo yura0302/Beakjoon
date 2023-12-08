49aae6325e5c43d2e1bb674a546a326fa4f8cb32
Yura <97827316+yura0302@users.noreply.github.com>
2023-12-09 00:03:00 +0900

    [Silver III] Title: N과 M (5), Time: 224 ms, Memory: 21112 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.js
@@ -0,0 +1,36 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+function solution(input) {
+  [n, m] = input[0].split(" ");
+  const N = Number(n);
+  const M = Number(m);
+  let answer = "";
+  const output = [];
+  const visited = new Array(N + 1).fill(false);
+  const num = input[1]
+    .split(" ")
+    .map(Number)
+    .sort((a, b) => a - b);
+
+  function dfs(count) {
+    if (count === M) {
+      answer += `${output.join(" ")}\n`;
+      return;
+    }
+    for (let i = 1; i <= N; i++) {
+      if (visited[i] === true) continue;
+      visited[i] = true;
+      output.push(num[i - 1]);
+      dfs(count + 1);
+      output.pop();
+      visited[i] = false;
+    }
+  }
+  dfs(0);
+  return answer;
+}
+console.log(solution(input));
