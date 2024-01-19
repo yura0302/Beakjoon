724ab20d9f68d1f2afc4cfbc6cfe5a3b4caf424e
Yura <97827316+yura0302@users.noreply.github.com>
2024-01-19 18:56:54 +0900

    [Gold IV] Title: 줄세우기, Time: 176 ms, Memory: 9624 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.js
@@ -0,0 +1,18 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const N = +input.shift();
+const arr = input.map((i) => +i);
+const longest = new Array(N).fill(1);
+
+for (let i = 1; i < N; i++) {
+  let cnt = 0;
+  for (let j = 0; j < i; j++) {
+    if (arr[j] < arr[i]) cnt = Math.max(cnt, longest[j]);
+  }
+  longest[i] = cnt + 1;
+}
+console.log(N - Math.max(...longest));
