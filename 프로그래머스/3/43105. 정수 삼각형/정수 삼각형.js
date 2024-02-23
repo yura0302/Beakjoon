fe7a37de52eeeff7236ebbb90dd2308b679f20f8
Yura <97827316+yura0302@users.noreply.github.com>
2024-02-23 23:51:12 +0900

    [level 3] Title: 정수 삼각형, Time: 4.50 ms, Memory: 41.7 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,9 +1,10 @@
 function solution(triangle) {
-    const dp =triangle.slice();
-    for(let i=dp.length-2; i>=0; i--){
+    let dp=triangle.slice();
+    
+    for(let i=triangle.length-2; 0<=i; i--){
         for(let j=0; j<dp[i].length; j++){
-            dp[i][j]+=Math.max(dp[i+1][j],dp[i+1][j+1])
+           dp[i][j]+= Math.max(dp[i+1][j],dp[i+1][j+1])
         }
     }
-    return dp[0][0]
+    return triangle[0][0];
 }
