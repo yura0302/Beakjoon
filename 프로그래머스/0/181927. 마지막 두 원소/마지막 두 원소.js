647beeee811f61d7b6523e33f1725c44f18da908
Yura <97827316+yura0302@users.noreply.github.com>
2024-07-03 17:10:45 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,9 +1,5 @@
 function solution(num_list) {
-    let answer=[];
-    if(num_list.at(-1)>num_list.at(-2)){
-        num_list.push(num_list.at(-1)-num_list.at(-2))
-    }else{
-        num_list.push(num_list.at(-1)*2)
-    }
+    num_list.at(-1)>num_list.at(-2) ? num_list.push(num_list.at(-1)-num_list.at(-2))
+    :num_list.push(num_list.at(-1)*2)
     return num_list;
 }
