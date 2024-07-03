a005017f1bc29f155d2807116b835cc9caff4e43
Yura <97827316+yura0302@users.noreply.github.com>
2024-07-03 18:24:36 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,7 +1,7 @@
 function solution(num_list) {
     const plus_num=num_list.reduce((acc,cur)=>acc+cur)
     const multiple_num=num_list.reduce((acc,cur)=>acc*cur)
-    const squared_num=Math.pow(plus_num,2)
+    const squared_num=plus_num**2
     if(multiple_num<squared_num){
         return 1
     }else{
