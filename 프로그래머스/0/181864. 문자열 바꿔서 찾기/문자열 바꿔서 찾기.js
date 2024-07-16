b08c56ab60aaf419ccae29b2d3c74b9c342bfbc9
Yura <97827316+yura0302@users.noreply.github.com>
2024-07-17 02:12:53 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -1,7 +1,8 @@
 function solution(myString, pat) {
-    let replaceMyString=[];
-    for (let i=0; i<myString.length; i++){
-        replaceMyString+=myString[i]=="A"? "B":"A"
-    }
-    return replaceMyString.includes(pat)?1:0
+    // let replaceMyString=[];
+    // for (let i=0; i<myString.length; i++){
+    //     replaceMyString+=myString[i]=="A"? "B":"A"
+    // }
+    // return replaceMyString.includes(pat)?1:0
+    return [...myString].map((s)=>s==="A"?"B":"A").join("").includes(pat)?1:0
 } 
