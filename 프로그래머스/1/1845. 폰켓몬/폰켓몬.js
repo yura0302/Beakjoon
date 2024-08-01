7bbfa4188043a8e7de64980bc8f3268ddbd1e216
Yura <97827316+yura0302@users.noreply.github.com>
2024-08-01 14:27:50 +0900

    [level 1] Title: 폰켓몬, Time: 0.60 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,12 +1,15 @@
 function solution(nums) {
-    let answer=[];
-    let temp=nums.length/2;
-    for(let i=0; i<nums.length; i++){
-        if(answer.length<temp){
-             if(!answer.includes(nums[i])){
-            answer.push(nums[i])
-            }
-        }
-    }
-    return answer.length;
+    // let answer=[];
+    // let temp=nums.length/2;
+    // for(let i=0; i<nums.length; i++){
+    //     if(answer.length<temp){
+    //          if(!answer.includes(nums[i])){
+    //         answer.push(nums[i])
+    //         }
+    //     }
+    // }
+    // return answer.length;
+    let arr=[...new Set(nums)]
+    let max=nums.length/2
+    return arr.length>max ? max : arr.length
 }
