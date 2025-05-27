57f3c196cd6f0ca45231e54ae5135f0e0d97e4b9
Yura <97827316+yura0302@users.noreply.github.com>
2025-05-27 23:50:38 +0900

    [Bronze V] Title: ??!, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.js
@@ -0,0 +1,4 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+// let input = fs.readFileSync("../input.txt").toString().trim().split(" ");
+console.log(input + "??!");
